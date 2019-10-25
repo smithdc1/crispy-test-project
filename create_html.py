278174d1cd8a0a18dc8642bc77cecf3fbf6a05f7
David Smith <39445562+smithdc1@users.noreply.github.com>
2019-10-25 16:40:15 +0100

    Update create_html.py

--- create_html.py
+++ create_html.py
@@ -1,16 +1,47 @@
 from django.test import Client
-from BeautifulSoup import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs
 import django
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
 django.setup()
 
+#get html
 c = Client()
 response = c.get('/bootstrap4')
-print(response.content)
 
+#make html pretty
 soup = bs(response.content)                #make BeautifulSoup
 prettyHTML = soup.prettify()
 print(prettyHTML)
+
+#env variable
+print(os.getenv('GITHUB_REF'))
+print(os.getenv('GITHUB_SHA'))
+print(os.getenv('GITHUB_WORKSPACE'))
+
+#create file name
+ref = os.getenv('GITHUB_REF')
+
+if 'pull' in ref:
+	start = ref.find(':')+1
+	end = ref.find('/',start)
+	ref= ref[start:end] + '-'
+	print(ref)
+
+else:
+	ref = 'push-'
+
+filename = ref + os.getenv('GITHUB_SHA') + '.html'
+
+print(filename)
+
+#make docs folder
+
+os.makedirs(os.getcwd() + '/docs/')
+path = os.getcwd() + filename
+print(path)
+
+with open("path", "w") as file:
+    file.write(prettyHTML)
 
