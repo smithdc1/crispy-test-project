8e7f29f7425506f1c8635a191750566a8d4a551a
David Smith <39445562+smithdc1@users.noreply.github.com>
2019-10-25 14:02:33 +0100

    Update create_html.py

--- create_html.py
+++ create_html.py
@@ -1,6 +1,8 @@
 from django.test import Client
+from BeautifulSoup import BeautifulSoup as bs
 import django
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
 django.setup()
 
@@ -8,4 +10,7 @@ c = Client()
 response = c.get('/bootstrap4')
 print(response.content)
 
+soup = bs(response.content)                #make BeautifulSoup
+prettyHTML = soup.prettify()
+print(prettyHTML)
 
