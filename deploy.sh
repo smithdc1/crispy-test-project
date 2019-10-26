2389cd374f2df79b71f43b83e11bb45b95f61a57
David Smith <39445562+smithdc1@users.noreply.github.com>
2019-10-26 20:45:03 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -37,7 +37,7 @@ then
 fi
 
 # Directs the action to the the Github workspace.
-cd $GITHUB_WORKSPACE && \
+cd $GITHUB_WORKSPACE/build && \
 
 # Configures Git.
 git init && \
@@ -62,7 +62,7 @@ then
 fi
 
 # Checks out the base branch to begin the deploy process.
-#git checkout "${BASE_BRANCH:-master}" && \
+git checkout "${BASE_BRANCH:-master}" && \
 
 
 # Commits the data to Github.
