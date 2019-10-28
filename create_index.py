f78fcbba7233b900b76173b7fe61935a5b729067
David Smith <smithdc@gmail.com>
2019-10-28 17:39:31 +0000

    corrected typo in deploy.sh

---
+++ create_index.py
@@ -0,0 +1,26 @@
+from yattag import Doc
+from bs4 import BeautifulSoup
+import os
+
+
+doc, tag, text = Doc().tagtext()
+
+file_list = os.listdir('/home/runner/build/docs/')
+file_list.sort(reverse=True)
+
+with tag('html'):
+    with tag('body'):
+        with tag('H1'):
+            text('Django-crispy-forms')
+
+        for item in file_list:
+            with tag('a', href='docs/'+item):
+                text(item)
+                with tag('br'):
+                    text('')
+
+
+soup = BeautifulSoup(doc.getvalue(), 'html.parser').prettify()
+
+with open('/home/runner/build/index.html', 'w') as file:
+    file.write(soup)
